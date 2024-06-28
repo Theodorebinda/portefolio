b7f5431c1d6e96a3566a7ec8b47b914c18f0892a
Théodorebinda <theodorebinda@gmail.com>
2024-06-28 23:27:26 +0100

    update package

--- src/types/competences.tsx
+++ src/types/competences.tsx
@@ -1,10 +1,10 @@
 import { StaticImageData } from "next/image"
 
 export interface Competences {
-    id : number | string
-    image : string | StaticImageData 
-    name : string
-    description? : string
-    progression? : number |undefined
+    id : number | string;
+    image : string | StaticImageData ;
+    name : string;
+    description? : string;
+    progression? : number | undefined;
   }
 
