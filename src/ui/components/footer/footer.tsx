ffbf4db6e94a179951279036b86a56d0c06e8023
Théodorebinda <theodorebinda@gmail.com>
2024-08-19 17:40:35 +0100

    update footer

--- src/ui/components/footer/footer.tsx
+++ src/ui/components/footer/footer.tsx
@@ -5,13 +5,13 @@ import { Typography } from "../typography/typography";
 export default function Footer (){
     return(
         <Container className=" md:min-w-full my-8 ">
-            <Container className="border-t border-[#545454] flex md:m-auto pt-4 md:w-1/2 mx-10">
+            <Container className="border-t border-[#545454] flex md:m-auto pt-4 md:w-1/2 mx-8">
                 
-                <Container className="flex flex-row justify-between items-center gap-2">
+                <Container className="flex flex-row justify-between md:m-auto  items-center gap-1">
                     <Copyright strokeWidth={2} size={20} />
-                    <Typography className="text-xs md:text-base">Theodore Samba Binda </Typography>
+                    <Typography className="text-xs ">Theodore Samba Binda </Typography>
                     <Dot strokeWidth={1.75} />
-                    <Typography className="text-xs md:text-base">Kinshasa - RDC</Typography>
+                    <Typography className="text-xs ">Kinshasa - RDC 2024</Typography>
 
                 </Container>
             </Container>
