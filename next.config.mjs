b203666fc113084ec1b6dee5d0b900ea86847914
Théodorebinda <theodorebinda@gmail.com>
2024-12-20 00:54:00 +0100

    fix:next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require('next-pwa')({
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-  });
-  
-  const nextConfig = {};
-  
-  module.exports = withPWA(nextConfig);
-  
+const withPWA = import("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig = {};
+
+module.exports = withPWA(nextConfig);
