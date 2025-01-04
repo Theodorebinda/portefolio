c1e413ac450d791105f9c4a78b27f1ee17ad8884
theodorebinda <theodorebinda@gmail.com>
2025-01-05 00:28:07 +0100

    fix:shadow navigation

--- src/app/(main-route)/competences/page.tsx
+++ src/app/(main-route)/competences/page.tsx
@@ -6,26 +6,28 @@ import { Typography } from "@/ui/components/typography/typography";
 
 export default function Competences() {
   return (
-    <Container className="md:h-screen  flex flex-col items-start justify-start text-start gap-8 text-xl font-normal leading-relaxed  lg:w-2/3 md:w-3/4">
-      <P>Autre Competences</P>
-      <Container className="flex flex-col gap-4">
-        <P>Electricien</P>
-        <Typography className="text-xl font-normal leading-relaxed">
-          {" Diplomé en Electricité,"}
-          <br />
-          {
-            "J'ai travaillé dans le domaine de l'électricité depuis 2014, spécialisé dans les installations electrique de batiment et maintenance d'equipement electrique"
-          }
-        </Typography>
-      </Container>
-      <Container className="flex flex-col gap-4">
-        <P>Instructeur</P>
-        <Typography className="text-xl font-normal leading-relaxed">
-          {
-            "Au paravant j'ai travaillé comme enseignant et responsable des ateliers ou je supervisais les travaux pratique et assurais la securité des eleves"
-          }
-          <br />
-        </Typography>
+    <Container className="md:h-screen  flex flex-col justify-between text-start text-xl font-normal leading-relaxed  lg:w-2/3 md:w-3/4">
+      <Container className="flex flex-col justify-between text-start gap-10">
+        <P>Autre Competences</P>
+        <Container className="flex flex-col gap-4">
+          <P>Electricien</P>
+          <Typography className="text-xl font-normal leading-relaxed">
+            {" Diplomé en Electricité,"}
+            <br />
+            {
+              "J'ai travaillé dans le domaine de l'électricité depuis 2014, spécialisé dans les installations electrique de batiment et maintenance d'equipement electrique"
+            }
+          </Typography>
+        </Container>
+        <Container className="flex flex-col gap-4">
+          <P>Instructeur</P>
+          <Typography className="text-xl font-normal leading-relaxed">
+            {
+              "Au paravant j'ai travaillé comme enseignant et responsable des ateliers ou je supervisais les travaux pratique et assurais la securité des eleves"
+            }
+            <br />
+          </Typography>
+        </Container>
       </Container>
       <LinkToOtherPage
         className=" text-lg"
