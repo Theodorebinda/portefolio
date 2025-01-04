cb4debeb46a3e1c62632bfee49b0ecaed50eb268
theodorebinda <theodorebinda@gmail.com>
2025-01-05 00:00:56 +0100

    fix

--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -8,17 +8,16 @@ import Link from "next/link";
 import { Btn, Typo } from "@/styles/globalStyle";
 import { ChevronRight } from "lucide-react";
 
+// src/components/heroSection.tsx
 export default function HeroSection() {
   return (
-    <Container className="flex flex-col md:items-start md:justify-between md:gap-10 md:flex-row  lg:h-screen ">
-      <Container className="flex flex-col gap-5  ">
-        <Typo className="font-normal text-7xl md:text-7xl ">
-          Théodore Samba
-        </Typo>
-        <Typography className="text-xl font-normal ">
+    <Container className="flex flex-col md:items-start md:justify-between md:gap-10 md:flex-row lg:h-screen">
+      <Container className="flex flex-col gap-5">
+        <Typo className="font-normal text-7xl md:text-7xl">Théodore Samba</Typo>
+        <Typography className="text-xl font-normal">
           Salut monde, je suis
         </Typography>
-        <Container className="">
+        <Container>
           <Typewriter
             texts={[
               "Developpeur web/mobile",
@@ -34,17 +33,12 @@ export default function HeroSection() {
             {" Créateur d'expériences digitales innovantes et passionnantes."}
           </span>
           <span>{"Transformant vos idées en réalités numériques."}</span>
-          {/* <span>{"passioné par le développement social"} </span>  */}
         </Typography>
-        <Container className=" mt-10 mb-4 flex justify-center p-2">
+        <Container className="mt-10 mb-4 flex justify-center p-2">
           <Link href={"/contact"}>
-            <Btn className=" flex justify-center items-center gap-1 px-5 py-6">
+            <Btn className="flex justify-center items-center gap-1 px-5 py-6">
               {"Contactez Moi"}
-        <Container className="mt-10 md:mt-20 flex justify-center p-2">
-          <Link href={"/contact"}>
-            <Btn className=" flex justify-center items-center gap-1 px-5 py-6">
-              {"Contactez Moi"}
-              <ChevronRight className=" w-5 h-5 font-light" />
+              <ChevronRight className="w-5 h-5 font-light" />
             </Btn>
           </Link>
         </Container>
@@ -58,17 +52,7 @@ export default function HeroSection() {
           className="object-cover"
           loading="lazy"
         />
-      <Container className="hidden md:basis-1/3  lg:flex lg:justify-center items-start w-1/2">
-        <Image
-          src={imageHero}
-          height={400}
-          width={400}
-          alt="theodore"
-          className="object-cover"
-          loading="lazy"
-        />
       </Container>
     </Container>
   );
 }
-// console.log(HeroSection);
