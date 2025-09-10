3a71b452224e2e3ce6f2e82b3011803f361c1d19
Theodoore samba <theodorebinda@gmail.com>
2025-09-10 04:16:46 +0100

    🛠️fix: Ajout de la prise en charge de la traduction pour les composants Carriere et AnnualSummary. Mise à jour des fichiers de langue pour l'anglais et le français, et refactorisation des données pour utiliser des clés de traduction.

--- src/components/annual-summary.tsx
+++ src/components/annual-summary.tsx
@@ -1,19 +1,21 @@
 // components/AnnualSummary.tsx
 "use client";
 
+import { useTranslation } from "@/lib/hooks/useTranslation";
 import { AnnualDataType } from "@/utils/data";
 import Image, { StaticImageData } from "next/image";
 import React, { useEffect, useState } from "react";
 
 const AnnualSummary: React.FC<AnnualDataType> = ({
   year,
-  title,
-  description,
-  achievements,
+  titleKey,
+  descriptionKey,
+  achievementsKeys,
   imageUrls,
 }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [fade, setFade] = useState(true);
+  const { t } = useTranslation();
 
   useEffect(() => {
     if (imageUrls.length === 0) return;
@@ -35,7 +37,7 @@ const AnnualSummary: React.FC<AnnualDataType> = ({
         <div className="w-full md:w-2/5 transition-all duration-500 ease-in-out h-[300px]  relative overflow-hidden rounded-xl -z-50">
           <Image
             src={imageUrls![currentIndex]}
-            alt={`Image ${currentIndex + 1} pour l'année ${year}`}
+            alt={`${t("image_for_year", { year })} ${currentIndex + 1}`} // Traduction
             fill
             className="object-cover"
             sizes="(max-width: 768px) 100vw, 40vw"
@@ -48,20 +50,22 @@ const AnnualSummary: React.FC<AnnualDataType> = ({
         <div className="flex flex-col items-start">
           <h3 className="text-sm uppercase tracking-wider mb-1">{year}</h3>
           <span className="text-3xl md:text-4xl font-bold text-[#436896] hover:text-[#5182be] font-poppins">
-            {title}
+            {t(titleKey)} {/* Traduction */}
           </span>
-          <p className="text-xl font-poppins leading-relaxed">{description}</p>
+          <p className="text-xl font-poppins leading-relaxed">
+            {t(descriptionKey)} {/* Traduction */}
+          </p>
         </div>
         <div>
-          {achievements?.length > 0 && (
+          {achievementsKeys?.length > 0 && (
             <div className="flex flex-col">
               <h5 className="text-lg font-semibold mb-2">
-                Réalisations clés :
+                {t("career.achievements_title")} {/* Traduction */}
               </h5>
               <ul className="list-disc list-inside space-y-1 ">
-                {achievements.map((item, index) => (
+                {achievementsKeys.map((achievementKey, index) => (
                   <li key={index} className="leading-snug">
-                    {item}
+                    {t(achievementKey)} {/* Traduction */}
                   </li>
                 ))}
               </ul>
