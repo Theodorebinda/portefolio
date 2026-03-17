f84a0bc2d2e3790e6e75daef89c945ebe04728b7
Theodoore samba <theodorebinda@gmail.com>
2026-03-17 10:00:10 +0100

    fix: mise à jour du projet

--- src/app/(main-route)/page.tsx
+++ src/app/(main-route)/page.tsx
@@ -16,7 +16,7 @@ const RealisationSection = dynamic(
     loading: () => <div className="min-h-[300px]" />,
   },
 );
-// hmfnhsndgjslkmd.adgnm,sfjgnldhjnmflg
+// hmfnhsndgjslkmd.adgnm,sfjgnldhjnmfl
 
 export default function Home() {
   useTrackPageView();
